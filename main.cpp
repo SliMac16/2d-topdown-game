b8e68b39417f2147ca40967d94cf596e33fc3c43
SliMac16 <maciej.sliwinski98@gmail.com>
2023-08-29 15:48:31 +0200

    Map and Character creation
    drawing map and character

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "raymath.h"
 
 
 int main(){
@@ -7,14 +8,40 @@ int main(){
     InitWindow(windowWidth,windowHeight, "Top Down Game");
 
     Texture2D map = LoadTexture("nature_tileset/WorldMap.png");
+    Vector2 mapPos{0.0,0.0};
+    float speed = 5.0;
+
+    Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
+    Vector2 knightPos{
+        windowWidth/2.0f - 4.0f * (0.5f * (float)knight.width/6.0),
+        windowHeight/2.0f - 4.0f * (0.5f * (float)knight.height),
+
+    };
 
     SetTargetFPS(60);
     while(!WindowShouldClose()){
         BeginDrawing();
         ClearBackground(WHITE);
 
-        Vector2 mapPos{0.0,0.0};
+        Vector2 direction{};
+        if(IsKeyDown(KEY_A)) direction.x -= 1.0;
+        if(IsKeyDown(KEY_D)) direction.x += 1.0;
+        if(IsKeyDown(KEY_W)) direction.y -= 1.0;
+        if(IsKeyDown(KEY_S)) direction.y += 1.0;
+
+        if(Vector2Length(direction) != 0.0){
+            
+            //set mapPos =mapPos - direction
+            mapPos = Vector2Subtract(mapPos,Vector2Scale(Vector2Normalize(direction),speed));
+        }
+        // draw the map
         DrawTextureEx(map, mapPos, 0.0, 4.0,WHITE);
+
+        //draw the character
+        Rectangle source{0.f, 0.f, (float)knight.width/6.f, (float)knight.height};
+        Rectangle dest{knightPos.x, knightPos.y, 4.0f * (float)knight.width/6.0f, 4.0f * (float) knight.height};
+        DrawTexturePro(knight, source, dest, Vector2{}, 0.f, BLACK);
+
         EndDrawing();
     }
 }
